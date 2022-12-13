bf3facf7700f28a96120c86bdffb50da1e075f59
shpali4 <shamardinvs@yandex.ru>
2022-12-14 01:22:29 +0700

    -first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение бинов HelloWorld: " + (bean == bean1));
+        System.out.println("Сравнение бинов Cat: "+ (cat == cat1));
     }
 }
